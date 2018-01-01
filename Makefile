f6619e960e0ada0d68da56d811db019a59414ba1
Owain Lewis <owain@owainlewis.com>
2018-01-01 08:20:16 +0000

    Basic client set stuff

--- Makefile
+++ Makefile
@@ -1,5 +1,14 @@
+.PHONY: build
 build:
 	@go build cmd/main.go
 
+.PHONY: clean
 clean:
 	@rm -rf bin
+
+.PHONY: run
+run:
+	@go run cmd/main.go \
+	--kubeconfig=${HOME}/.kube/config \
+	--v=4 \
+	--logtostderr=true
--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"flag"
 	"io"
-	"log"
 	"net/http"
+
+	"github.com/golang/glog"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -11,9 +16,41 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var kubeconfig = flag.String("kubeconfig", "", "Path to a kubeconfig file")
+
+	flag.Parse()
+
+	_, err := buildClient(*kubeconfig)
+	if err != nil {
+		glog.Errorf("Failed to build client: %s", err)
+		return
+	}
+
 	http.HandleFunc("/", index)
 
-	log.Println("Starting server on port 8080")
+	glog.Info("Starting server on port 8080")
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func buildClient(cnf string) (*kubernetes.Clientset, error) {
+	config, err := getKubeConfig(cnf)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func getKubeConfig(cnf string) (*rest.Config, error) {
+	if cnf != "" {
+		return clientcmd.BuildConfigFromFlags("", cnf)
+	}
+
+	return rest.InClusterConfig()
 }

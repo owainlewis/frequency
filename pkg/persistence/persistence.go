037fe625b701a6bf8e1fb79dba1fc7aa5321f581
Owain Lewis <owain@owainlewis.com>
2018-01-19 20:55:23 +0000

    Split interfaces

--- pkg/persistence/persistence.go
+++ pkg/persistence/persistence.go
@@ -2,8 +2,15 @@ package persistence
 
 import "github.com/owainlewis/kcd/pkg/types"
 
-// Datastore defines the interface for persisting types
-type Datastore interface {
+type JobStore interface {
 	GetJob(id string) (*types.Job, error)
 	SaveJob(job types.Job) error
+}
+
+type ProjectStore interface {
+}
+
+// Datastore defines the interface for persisting types
+type Datastore interface {
+	JobStore
 }
